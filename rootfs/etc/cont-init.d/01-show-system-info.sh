b0fb2fb8bc14769d6f0045d1444b07d14321d827
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-12 02:47:47 -0300

    Create 01-show-system-info.sh

---
+++ rootfs/etc/cont-init.d/01-show-system-info.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bash
+echo "########################### OPERATION SYSTEM INFO ###########################"
+
+cat /etc/*-release
+
+uname -a
+
+cat /proc/version
+
+echo "#############################################################################"
